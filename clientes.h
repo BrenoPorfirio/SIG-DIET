791c695955d19f489320323cb56e7956498f984c
BrenoPorfirio <brenoporfirio03@gmail.com>
2022-10-30 14:57:09 -0300

    Struct Clientes(Pt.2.1)

--- clientes.h
+++ clientes.h
@@ -1,4 +1,12 @@
 char modulo_clientes(void);
+typedef struct cliente Cliente;
+struct cliente{
+	char cpf[12];
+	char nome[61];
+	char telefone[20];
+	char email[40];
+};
+Cliente *cadastro();
 void modulo_cad_clientes(void);
 void modulo_ver_clientes(void);
 void modulo_mod_clientes(void);
